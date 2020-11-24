694aa4d99c98976a67b2ce88d002e0c6f4f46695
Salim <salimp2009@gmail.com>
2020-11-24 15:50:12 +0300

    added Gameplay ability plugin and Gameplaytags; added to c++ and build.cs

--- Source/AbilitySystem/Private/CharacterBase.cpp
+++ Source/AbilitySystem/Private/CharacterBase.cpp
@@ -8,7 +8,7 @@ ACharacterBase::ACharacterBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	AbilitySystemComp = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComp");
 }
 
 // Called when the game starts or when spawned
@@ -22,7 +22,7 @@ void ACharacterBase::BeginPlay()
 void ACharacterBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	//
 }
 
 // Called to bind functionality to input
@@ -30,5 +30,23 @@ void ACharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+UAbilitySystemComponent* ACharacterBase::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComp;
+}
+
+void ACharacterBase::AcquireAbility(TSubclassOf<UGameplayAbility> AbilityToAcquire)
+{
+	if (AbilitySystemComp)
+	{
+		if (HasAuthority() && AbilityToAcquire)
+		{
+			AbilitySystemComp->GiveAbility(FGameplayAbilitySpec(AbilityToAcquire, 1, 0));
+		}
+		
+		AbilitySystemComp->InitAbilityActorInfo(this, this);
+	}
 }
 
--- Source/AbilitySystem/Public/CharacterBase.h
+++ Source/AbilitySystem/Public/CharacterBase.h
@@ -1,13 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystemComponent.h"
+#include "Abilities/GameplayAbility.h"
 #include "CharacterBase.generated.h"
 
 UCLASS()
-class ABILITYSYSTEM_API ACharacterBase : public ACharacter
+class ABILITYSYSTEM_API ACharacterBase : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
@@ -26,4 +27,13 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "CharacterBase")
+	UAbilitySystemComponent* AbilitySystemComp;
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const;
+
+	UFUNCTION(BlueprintCallable, Category = "CharacterBase")
+	void AcquireAbility(TSubclassOf<UGameplayAbility> AbilityToAcquire);
+
+	
 };
