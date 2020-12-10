3fccf7d960c0a6625369b34463280788bc1045ef
Salim <salimp2009@gmail.com>
2020-12-10 13:45:30 +0300

    added c++ GameplaAbilityclass; added GameplayTasks to build.cs; added AbilityType C++ Actor class ; deleted all the class in c++ and implemented USTRUCT() so we can pass all info needed to to use in our UI from GameplayAbility to USTRUCT() and use it in BP and UserInterface; NEXT need to add function to C++ to get the info into USTRUCT()

---
+++ Source/AbilitySystem/Public/GameplayAbilityBase.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GameplayAbilityBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABILITYSYSTEM_API UGameplayAbilityBase : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AbilityBase")
+	UMaterialInstance* UIMaterial;
+	
+};
